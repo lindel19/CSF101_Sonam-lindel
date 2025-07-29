702feb4c65fe201d56e6feae22423d13bc9c2d9d
Sonam Lindel <Add_Your_Email_Here>
2025-07-29 14:36:18 +0600

    first_programe.py

--- first_programe.py
+++ first_programe.py
@@ -1,2 +1,12 @@
 print('hello world')
-print('hello swe 2025')
+a=5
+b=14
+sum=a+b
+print('sum is', sum)
+if sum > 10:
+    print('sum is greater than 10')
+else:
+    print('sum is not greater than 10')
+name = input('enter your name:')
+age = int(input('enter your age:'))
+print('you are', name, 'and you are', age, 'years old')
