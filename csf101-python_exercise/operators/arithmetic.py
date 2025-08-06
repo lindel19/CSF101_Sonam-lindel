3b3c0175970b250ba272c9e6d0ecca1cc6bf4c47
Sonam Lindel <Add_Your_Email_Here>
2025-08-06 23:37:38 +0600

    arithmetic.py

---
+++ csf101-python_exercise/operators/arithmetic.py
@@ -0,0 +1,12 @@
+a , b = 19 , 10 
+print(f"a={a}, b = {b}")
+print(f"addition: {a+b}")
+print(f"subtraction: {a-b}")
+print(f"multiplication: {a*b}")
+print(f"division: {a/b}")
+print(f"modulus: {a % b}")
+print(f"exponential: {a ** b}")
+print(f"floor division: {a // b}")
+
+                      
+                      
