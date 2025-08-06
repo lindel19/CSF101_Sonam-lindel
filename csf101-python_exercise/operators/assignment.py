c7bef2c2e7bc590f0ab835838263edf031e89592
Sonam Lindel <Add_Your_Email_Here>
2025-08-06 23:53:47 +0600

    assignment.py

---
+++ csf101-python_exercise/operators/assignment.py
@@ -0,0 +1,12 @@
+x = 19
+print(f"intial x: {x}")
+x += 5
+print(f"x after += 5: {x}")
+x -= 3
+print(f"x after -= 3: {x}")
+x *= 2 
+print(f"x after *= 2: {x}")
+x /= 4
+print(f"x after /= 4: {x}")
+
+
