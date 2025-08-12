ba563345311eacdf2790eeebeed89b04f34307b1
Sonam Lindel <Add_Your_Email_Here>
2025-08-12 14:48:52 +0600

    loops.py

---
+++ csf101-python_exercise/control_structures/loops.py
@@ -0,0 +1,18 @@
+for i in range (1 , 6):
+    print(i)
+count = 5 
+while count > 0:
+    print(count)
+    count-= 1
+total = 0
+for num in range(1, 20):
+    total+= num 
+print(f"the sum of numbers from 1 to 20 is: {total}") 
+fruits = ["apple", "banana", "cherry"] 
+for fruit in fruits:
+    print(fruit)
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(f"{i}*{j} = {i * j}")
+    print()
+    
